f04c89b70f4c8a29fceee4909c075e60ac510b49
Bryana Ojopi <bojopi@scz.prefectura.gov.bo>
2023-10-12 14:59:27 -0400

    finalizacion de la primera seccion

--- src/app/dbz/pages/main_page.component.ts
+++ src/app/dbz/pages/main_page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DBZService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
 
 export class MainPageComponent {
 
+  constructor( private dbzService: DBZService ) {}
 
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
 
+  onDeleteCharacter(id: string): void {
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  onNewCharacter(character: Character): void {
+    this.dbzService.addCharacter(character);
+  }
 }
